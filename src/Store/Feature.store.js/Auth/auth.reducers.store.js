6482d63dc96e666daac2b971406d362ca89a8c5e
verzeo <tech@verzeo.com>
2023-06-26 00:41:31 +0530

    neccessary changes

--- src/Store/Feature.store.js/Auth/auth.reducers.store.js
+++ src/Store/Feature.store.js/Auth/auth.reducers.store.js
@@ -6,17 +6,11 @@ export const authSlice = createSlice({
     initialState: INITIAL_AUTH,
     reducers: {
         setUserDetails: (state) => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the Immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
             state.value += 1;
         },
     },
 });
 
+export const { setUserDetails } = authSlice.actions;
 
-// Action creators are generated for each case reducer function
-export const { setUserDetails } = authSlice.actions
-
-export default authSlice.reducer
+export default authSlice.reducer;
